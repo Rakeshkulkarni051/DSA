de8c31292aa6d365801aad41a935c2a270390bf8
Rakesh <rakeshkulkarni403@gmail.com>
2026-05-27 19:19:36 +0530

    pattern updates

--- Patterns/oddnotria.java
+++ Patterns/oddnotria.java
@@ -4,6 +4,15 @@ public class oddnotria {
         // 13
         // 135
         // 1357
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){ //j<=2*i
+                // if(j%2==1){
+                     System.out.print(" " +(2*j-1));
+               // } 
+            }
+            System.out.println("");
+        }
 
     }
 }
