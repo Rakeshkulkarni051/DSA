27a6ca6846473083b9c8be0ed767cdf085ebe7b7
Rakesh <rakeshkulkarni403@gmail.com>
2026-05-28 18:36:04 +0530

    new push to patterns

---
+++ Patterns/Diamond.java
@@ -0,0 +1,44 @@
+public class Diamond{
+    public static void main(String[] args) {
+        int n=8;
+
+        // Method 1 - NSP , NST 
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Method 2 - formula based
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i+1;j++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int k=1;k<=2*i-1;k++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println("");
+        // }
+        // for(int i = n -1; i >= 1; i--){
+
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print("  ");
+        //     }
+
+        //     for(int k=1;k<=2*i-1;k++){
+        //         System.out.print(" *");
+        //     }
+        //     System.out.println("");
+        // }
+    }
+}
