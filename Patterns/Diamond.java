4b5cd5e54f080cea28219ab55eb4fbeae08c8145
Rakesh <rakeshkulkarni403@gmail.com>
2026-05-28 18:36:37 +0530

    diamond pattern mth 2

--- Patterns/Diamond.java
+++ Patterns/Diamond.java
@@ -4,8 +4,33 @@ public class Diamond{
 
         // Method 1 - NSP , NST 
 
-        
+        int nsp=n-1,nst=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=nst;k++){
+                System.out.print("* ");
+            }
+            nsp--;
+            nst=nst+2;
+            System.out.println("");
+        }
 
+        nsp=1;
+        nst=2*n-3;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=nst;k++){
+                System.out.print("* ");
+            }
+            nsp++;
+            nst=nst-2;
+            System.out.println("");
+        }
 
 
 
