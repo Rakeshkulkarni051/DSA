9e770d93e2eba6af42abc047213bf93f470d39eb
Rakesh <rakeshkulkarni403@gmail.com>
2026-06-04 00:32:12 +0530

    Secound largest array push

---
+++ Arrays/secound_largest.java
@@ -0,0 +1,23 @@
+public class secound_largest {
+    public static void main(String[] args) {
+        int arr[]={2,5,3,8,10,6};
+
+        int max=Integer.MIN_VALUE;
+        int sec_max=Integer.MIN_VALUE;
+
+        // find max val 
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>sec_max && arr[i]!=max){
+                sec_max=arr[i];
+            }
+        }
+        
+        System.out.println("Secound Largest element is : "+sec_max);
+    }
+}
