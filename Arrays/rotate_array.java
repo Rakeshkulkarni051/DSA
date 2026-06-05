97c52a0322738d68a7b5ea344ae18fd40ff4958d
Rakesh <rakeshkulkarni403@gmail.com>
2026-06-05 13:25:55 +0530

    array push

--- Arrays/rotate_array.java
+++ Arrays/rotate_array.java
@@ -1,5 +1,10 @@
 public class rotate_array {
     public static void main(String[] args) {
-        
+        int[] arr1={3,4,6,2,8,11};
+      //  int i=arr1[0];
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(""+arr1[i]);
+        }
     }
 }
