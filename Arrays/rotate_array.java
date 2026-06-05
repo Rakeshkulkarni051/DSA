5a4034a76ee89231ffc5ca224ed4218b4f834f46
Rakesh <rakeshkulkarni403@gmail.com>
2026-06-05 13:44:42 +0530

    rotate array

--- Arrays/rotate_array.java
+++ Arrays/rotate_array.java
@@ -1,10 +1,22 @@
 public class rotate_array {
     public static void main(String[] args) {
-        int[] arr1={3,4,6,2,8,11};
-      //  int i=arr1[0];
-        for(int i=0;i<=5;i++)
-        {
-            System.out.println(""+arr1[i]);
+        int[] arr={3,4,6,2,8,11};
+        int r=3;
+        int arr_cpy[]=new int[arr.length-r];
+        int cpy_index=0;
+
+        for(int i=r;i<arr.length;i++){
+            arr_cpy[cpy_index]=arr[i];
+            cpy_index++;
         }
+
+        for(int ele:arr_cpy){
+            System.out.print(" "+ ele);
+        }
+
+        for(int i=0;i<r;i++){
+            System.out.print(" "+arr[i]);
+        }
+
     }
 }
