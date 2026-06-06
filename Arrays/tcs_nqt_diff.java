797875835adf71477874c4d5b609727a0ca46cbe
Rakesh <rakeshkulkarni403@gmail.com>
2026-06-06 19:53:52 +0530

    fixes in array

--- Arrays/tcs_nqt_diff.java
+++ Arrays/tcs_nqt_diff.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class tcs_nqt_diff {
     public static void main(String[] args) {
-        // given 2 1 assending and decending array make new array merge both in assending 
+        // given 2 array 1 assending and 1 decending array make new array merge both in assending 
         int a[]={1,3,4,7,11,14};
         int b[]={12,9,8,5,2,0};
         int size=a.length+b.length;
