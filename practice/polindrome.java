e66e28729409ffc8a0922e5871ee1a9ab8f89b31
Rakesh <rakeshkulkarni403@gmail.com>
2026-06-25 22:38:29 +0530

    practice set

---
+++ practice/polindrome.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class polindrome{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number to check for polindrome");
+        int no=sc.nextInt();
+        
+
+        sc.close();
+
+        
+    }
+}
