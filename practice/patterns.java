c38e47c8bc820ae7020ab65615ca7102849327fa
Rakesh <rakeshkulkarni403@gmail.com>
2026-08-01 00:27:15 +0530

    Practice set

--- practice/patterns.java
+++ practice/patterns.java
@@ -30,6 +30,29 @@ public class patterns {
     //         System.out.println("");
     //     }
     
+    // int n=5;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print("  ");
+    //     }
+    //     for(int k=1;k<=i;k++){
+    //         System.out.print(" "+k);
+    //     }
+    //     System.out.println("");
+    // }
+     //hortizontal verticle flipped 
+     int n=5;
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("  ");
+        }
+        for(int k=1;k<=n-i+1;k++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+     }
+
+
 
     }
 }
