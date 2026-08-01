fc79c9c4c3c0799a3789fa925d138d390657e89f
Rakesh <rakeshkulkarni403@gmail.com>
2026-08-02 03:10:14 +0530

    end of the day

--- practice/mininarray.java
+++ practice/mininarray.java
@@ -1,5 +1,14 @@
 public class mininarray {
     public static void main(String args[]){
-        
+        int arr[]={3,5,4,7,10};  
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println("min value in array is : "+min);
+
+
     }
 }
