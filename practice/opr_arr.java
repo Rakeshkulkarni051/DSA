b5157824e15dc3f2e80fa06cf2f8e07992e14544
Rakesh <rakeshkulkarni403@gmail.com>
2026-08-02 02:15:23 +0530

    arrays brush ups

--- practice/opr_arr.java
+++ practice/opr_arr.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class opr_arr {
     public static void main(String args[]){
         //int arr[]=new int[5];
@@ -7,7 +8,21 @@ public class opr_arr {
         // arr[1]=10;
         // arr[0]=34;
         // System.out.println(arr[6]);
-        
 
+
+        System.out.println("Enter size of array: ");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter elements into array max "+size);
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("Elements in the array : ");
+        for(int s : arr){
+            System.out.printf("%d \t",s);
+        }
+        sc.close();
     }
 }
