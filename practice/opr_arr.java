b7b14e8609a5b3b1635fdb241743f2ad0ade0bea
Rakesh <rakeshkulkarni403@gmail.com>
2026-08-02 01:51:55 +0530

    array brush ups

---
+++ practice/opr_arr.java
@@ -0,0 +1,13 @@
+public class opr_arr {
+    public static void main(String args[]){
+        //int arr[]=new int[5];
+        //int arr[]={2,4,5,61,2};
+        // int arr[];
+        // arr=new int[5];
+        // arr[1]=10;
+        // arr[0]=34;
+        // System.out.println(arr[6]);
+        
+
+    }
+}
