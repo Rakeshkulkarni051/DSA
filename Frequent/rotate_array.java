26c6ca3960d4c32faef78b3394423b75e05d372e
Rakesh <rakeshkulkarni403@gmail.com>
2026-08-05 00:27:57 +0530

    rotoate array

--- Frequent/rotate_array.java
+++ Frequent/rotate_array.java
@@ -1,5 +1,26 @@
-package Frequent;
+//package Frequent;
 
 public class rotate_array {
-    
+
+    public static void main(String args[]){
+
+    int ar[]={2,1,4,2,6,12,5,7,3};
+    // start n end positions 
+    int start=3,end=6;
+
+    int i=start;
+    int j=end;
+
+    while(i<j){
+        int temp=ar[i];
+        ar[i]=ar[j];
+        ar[j]=temp;
+        i++;
+        j--;
+    }
+
+    for(int ele:ar){
+        System.out.println(ele);
+    }
+}
 }
